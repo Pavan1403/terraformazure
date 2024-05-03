409f16296964dcb824f68186af6e4c7f5d0de275
Pavan1403 <93857647+Pavan1403@users.noreply.github.com>
2024-05-03 16:59:44 +0530

    Update main.tf

--- Azure_Example_Full/main.tf
+++ Azure_Example_Full/main.tf
@@ -3,19 +3,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      source = "hashicorp/azurerm"
+      version = "3.100.0"
     }
   }
 }
-
-#https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_client_secret
 provider "azurerm" {
-  features {} 
-  client_id       = "00000000-0000-0000-0000-000000000000"
-  client_secret   = "20000000-0000-0000-0000-000000000000"
-  tenant_id       = "10000000-0000-0000-0000-000000000000"
-  subscription_id = "20000000-0000-0000-0000-000000000000"
+    features {}
+
+    subscription_id     = "97207ec2-cd27-4d22-ac63-1fbeb779e3bd"
+    client_id           = "cfa6c488-b42d-4a23-ad17-bcfa154a7802"
+    client_secret       = "KHI8Q~BI8QKnaHZldrmJzFZ0TvfbZgDgxftoFdaZ"
+    tenant_id           = "3603ef83-cbad-4339-81e2-23ac7a5aa90c"
+ 
 }
 
 #create resource group
@@ -47,9 +47,9 @@ resource "azurerm_network_security_group" "nsg1" {
   location            = "${azurerm_resource_group.rg.location}"
 }
 
-# NOTE: this allows RDP from any network
+# NOTE: this allows SSH from any network
 resource "azurerm_network_security_rule" "rdp" {
-  name                        = "rdp"
+  name                        = "ssh"
   resource_group_name         = "${azurerm_resource_group.rg.name}"
   network_security_group_name = "${azurerm_network_security_group.nsg1.name}"
   priority                    = 102
@@ -57,7 +57,7 @@ resource "azurerm_network_security_rule" "rdp" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "3389"
+  destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
 }
@@ -79,19 +79,20 @@ resource "azurerm_network_interface" "nic1" {
   }
 }
 
-resource "azurerm_windows_virtual_machine" "main" {
-  name                            = "${var.prefix}-vmt01"
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "${var.prefix}-vm01"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
   size                            = "Standard_B1s"
   admin_username                  = "adminuser"
   admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false  # Disable password authentication
   network_interface_ids = [ azurerm_network_interface.nic1.id ]
 
   source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2012-R2-Datacenter"
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
